a3b58277087c2b6bd56ad9cbc62975c4d1952371
Simon Convent <simon.convent@rwth-aachen.de>
2017-09-28 15:27:15 +0200

    Test return addresses in ompt cancel_parallel testcase

--- runtime/test/ompt/cancel/cancel_parallel.c
+++ runtime/test/ompt/cancel/cancel_parallel.c
@@ -10,12 +10,22 @@ int main()
   {
     if(omp_get_thread_num() == 0)
     {
+      printf("%" PRIu64 ": fuzzy_address=0x%lx or 0x%lx\n", ompt_get_thread_data()->value, ((uint64_t)(char*)(&& ompt_label_1))/256-1, ((uint64_t)(char*)(&& ompt_label_1))/256);
       #pragma omp cancel parallel
+      {} /* Empty block between "#pragma omp ..." and __asm__ statement as a workaround for icc bug */
+      __asm__("nop"); /* provide an instruction as jump target (compiler would insert an instruction if label is target of a jmp ) */
+ompt_label_1:
+      __asm__("nop"); 
     }
     else
     {
       usleep(100);
+      printf("%" PRIu64 ": fuzzy_address=0x%lx or 0x%lx\n", ompt_get_thread_data()->value, ((uint64_t)(char*)(&& ompt_label_2))/256-1, ((uint64_t)(char*)(&& ompt_label_2))/256);
       #pragma omp cancellation point parallel
+      {} /* Empty block between "#pragma omp ..." and __asm__ statement as a workaround for icc bug */
+      __asm__("nop"); /* provide an instruction as jump target (compiler would insert an instruction if label is target of a jmp ) */
+ompt_label_2:
+      __asm__("nop");
     }
   }
 
@@ -26,8 +36,11 @@ int main()
 
   // CHECK: {{^}}0: NULL_POINTER=[[NULL:.*$]]
   // CHECK: {{^}}[[MASTER_ID:[0-9]+]]: ompt_event_task_create: parent_task_id=[[PARENT_TASK_ID:[0-9]+]], parent_task_frame.exit=[[NULL]], parent_task_frame.reenter=[[NULL]], new_task_id=[[TASK_ID:[0-9]+]], codeptr_ra=[[NULL]], task_type=ompt_task_initial=1, has_dependences=no
-  // CHECK: {{^}}[[MASTER_ID]]: ompt_event_cancel: task_data=[[TASK_ID:[0-9]+]], flags=ompt_cancel_parallel|ompt_cancel_activated=17, codeptr_ra={{0x[0-f]*}}
-  // CHECK: {{^}}[[OTHER_THREAD_ID:[0-9]+]]: ompt_event_cancel: task_data=[[TASK_ID:[0-9]+]], flags=ompt_cancel_parallel|ompt_cancel_detected=33, codeptr_ra={{0x[0-f]*}}
+  // CHECK: {{^}}[[MASTER_ID]]: fuzzy_address={{.*}}[[RETURN_ADDRESS:0x[0-f]+]]
+  // CHECK: {{^}}[[MASTER_ID]]: ompt_event_cancel: task_data=[[TASK_ID:[0-9]+]], flags=ompt_cancel_parallel|ompt_cancel_activated=17, codeptr_ra=[[RETURN_ADDRESS]]{{[0-f][0-f]}}
+
+  // CHECK: {{^}}[[THREAD_ID:[0-9]+]]: fuzzy_address={{.*}}[[OTHER_RETURN_ADDRESS:0x[0-f]+]]
+  // CHECK: {{^}}[[THREAD_ID]]: ompt_event_cancel: task_data=[[TASK_ID:[0-9]+]], flags=ompt_cancel_parallel|ompt_cancel_detected=33, codeptr_ra=[[OTHER_RETURN_ADDRESS]]{{[0-f][0-f]}}
 
   return 0;
 }
